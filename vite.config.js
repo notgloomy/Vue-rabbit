925c850f25e3f694d42e5190f257cec8c4f1234f
not gloomy <2218009172.qq.com>
2023-11-29 11:32:05 +0800

    elementPlus自动按需导入

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 配置elementPlus按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
+    // 实际的路径转换 @ -> src
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
