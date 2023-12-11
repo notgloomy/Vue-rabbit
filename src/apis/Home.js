e7ec52966092a0ead55d425626c71a113ed42907
not gloomy <2218009172.qq.com>
2023-12-11 15:20:45 +0800

    home-goodItems组件封装

--- src/apis/Home.js
+++ src/apis/Home.js
@@ -2,3 +2,12 @@ import  httpInstance  from '@/utils/http'
 
 // 获取banner轮播图数据
 export const getBannerAPI = () => httpInstance.get('/home/banner')
+
+// 获取新鲜好物
+export const getNewAPI = () => httpInstance.get('/home/new')
+
+// 获取人气推荐
+export const getHotAPI = () => httpInstance.get('/home/hot')
+
+// 获取所有商品
+export const getGoodsAPI = () => httpInstance.get('/home/goods')
--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
 // 引入初始化样式文件
 import '@/styles/common.scss'
 
+// 引入懒加载指令插件并注册
+import { lazyPlugin } from '@/directives'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
