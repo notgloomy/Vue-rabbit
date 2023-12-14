cc84f576f15895bc2ca9e00f52a9f31c6a9ceb34
not gloomy <2218009172.qq.com>
2023-12-14 10:49:29 +0800

    放大镜效果-镜头跟随鼠标移动

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,6 +3,7 @@ import { getDetail } from '@/apis/detail';
 import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 import DetailHot from './components/DetailHot.vue'
+import ImageView from '@/components/ImageView/index.vue'
 
 const goods = ref({})
 const route = useRoute()
@@ -40,7 +41,7 @@ getGoods()
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+                <ImageView></ImageView>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
