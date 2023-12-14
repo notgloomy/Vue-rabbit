a3a44b174ab562c82685dc5c8c331c7b02d2e2de
not gloomy <2218009172.qq.com>
2023-12-14 16:17:15 +0800

    登录-表单校验-自定义校验规则

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -14,6 +14,20 @@ const rules = {
     password:[
         { required: true, message:'密码不能为空', trigger:'blur' },
         { min: 6,max:14,message: '密码长度为6-14个字符',trigger:'blur' }
+    ],
+    agree:[
+        {
+            validator:(rule,value,callback)=>{
+                console.log(value)
+                // 自定义校验逻辑
+                // 勾选就通过 不勾选就不通过
+                if(value){
+                    callback()
+                } else {
+                    callback(new Error('请勾选协议'))
+                }
+            }
+        }
     ]
 }
 </script>
@@ -48,8 +62,8 @@ const rules = {
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password"/>
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item label-width="22px" prop="agree">
+                <el-checkbox  size="large" v-model="form.agree">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
