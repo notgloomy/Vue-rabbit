9810574ecb2d555e0c377a5ce6ccb09c9360d589
not gloomy <2218009172.qq.com>
2023-12-14 18:57:00 +0800

    登录基本业务实现

---
+++ src/apis/user.js
@@ -0,0 +1,14 @@
+// 封装所有和用户相关的接口函数
+import httpInstance from "@/utils/http"
+
+//登录接口
+export const loginAPI = ({ account,password }) =>{
+    return httpInstance({
+        url:'/login',
+        method:'POST',
+        data: {
+            account,
+            password
+        }
+    })
+}
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,10 +1,15 @@
 <script setup>
 // 表单校验 (账号名+密码)
 import { ref } from 'vue'
+import { loginAPI } from '@/apis/user'
+import { ElMessage } from 'element-plus'
+import 'element-plus/theme-chalk/el-message.css'
+import { useRouter } from 'vue-router'
 // 1.准备表单对象
 const form = ref({
     account: '',
-    password: ''
+    password: '',
+    agree: true
 })
 // 2.准备规则对象
 const rules = {
@@ -33,13 +38,21 @@ const rules = {
 
 // 3.获取form实例做统一校验
 const formRef = ref(null)
+const router = useRouter()
 const doLogin = () => {
+    const { account, password } = form.value
     // 调用实例方法
-    formRef.value.validate((valid)=>{
+    formRef.value.validate(async (valid)=>{
         // valid: 所有表单都通过校验 才为true
         console.log(valid)
         if(valid){
             // TODO LOGIN
+           const res = await loginAPI({account, password})
+           console.log(res)
+           // 1.提示用户
+           ElMessage({ type: 'success', message:'登录成功' })
+           // 2.跳转首页
+           router.replace({path:'/'})
         }
     })
 }
