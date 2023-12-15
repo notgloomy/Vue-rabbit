f987444780b11a2da7c1879c33789f71246abdd8
not gloomy <2218009172.qq.com>
2023-12-15 15:07:57 +0800

    登录-token拦截器中携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
+import { useUserStore } from '@/stores/user'
 // axios基础的封装
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -9,7 +10,13 @@ const httpInstance = axios.create({
 
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
-    
+    // 1.从pinia中获取token数据
+    const userStore = useUserStore()
+    // 2.按照后端的要求拼接token数据
+    const token = userStore.userInfo.token
+    if(token){
+      config.headers.Authorization = `Bearer ${token}`
+    }
     return config
   }, e => Promise.reject(e))
   
