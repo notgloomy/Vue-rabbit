9ab2e348ee7281da73937f07f5b6f4b2affaa1c2
not gloomy <2218009172.qq.com>
2023-12-16 17:54:12 +0800

    头部购物车-内部删除功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -20,8 +20,17 @@ export const useCartStore = defineStore('cart', () => {
             cartList.value.push(goods)
         }
     }
+
+    const delCart = (skuId) => {
+        // 思路
+        // 1.找到要删除项的下标值 - splice
+        // 2.使用数组的过滤方法 - filter
+        const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+        cartList.value.splice(idx, 1)
+    }
     return {
         cartList,
-        addCart
+        addCart,
+        delCart
     }
 })
