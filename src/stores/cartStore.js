f3701c6e1426358b638410aa9ddff743e8f2748e
not gloomy <2218009172.qq.com>
2023-12-17 11:27:46 +0800

    本地购物车-列表购物车-单选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -29,6 +29,14 @@ export const useCartStore = defineStore('cart', () => {
         cartList.value.splice(idx, 1)
     }
 
+
+    // 单选功能
+    const singleCheck = (skuId, selected) => {
+        // 通过skuId找到要修改的那一项,然后把它的selected修改为传过来的值
+        const item = cartList.value.find((item) => item.skuId === skuId)
+        item.selected = selected
+    }
+
     // 计算属性
     // 1.总的数量  所有项的count之和
     const allCount = computed(() =>cartList.value.reduce((a,c) => a + c.count,0))
@@ -39,6 +47,10 @@ export const useCartStore = defineStore('cart', () => {
         addCart,
         delCart,
         allCount,
-        allPrice
+        allPrice,
+        singleCheck
     }
-})
+},{
+    persist: true
+}
+)
