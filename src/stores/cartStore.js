db926398ab71638d846cec34b86abd52528e00ee
not gloomy <2218009172.qq.com>
2023-12-17 16:11:39 +0800

    本地购物车-列表购物车-统计计算

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -43,6 +43,10 @@ export const useCartStore = defineStore('cart', () => {
     // 2.总的价格  所有项的count*price之和
     const allPrice = computed(() =>cartList.value.reduce((a,c) => a + c.count*c.price,0))
     
+    // 3.已选择数量
+    const selectedCount = computed(() => cartList.value.filter(item => item.selected).reduce((a,c) => a + c.count,0))
+    // 4.已选择商品价钱合计
+    const selectedPrice = computed(() => cartList.value.filter(item => item.selected).reduce((a,c) => a + c.count*c.price,0))
     // 是否全选
     const isAll = computed(() => cartList.value.every((item) => item.selected))
     
@@ -59,7 +63,9 @@ export const useCartStore = defineStore('cart', () => {
         allPrice,
         singleCheck,
         isAll,
-        allCheck
+        allCheck,
+        selectedCount,
+        selectedPrice
     }
 },{
     persist: true
