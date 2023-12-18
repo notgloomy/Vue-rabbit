b1ebe69528edf49f78197399b8b6d11472d5bacd
not gloomy <2218009172.qq.com>
2023-12-18 20:15:40 +0800

    订单-使用tab切换获取状态数据

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -25,11 +25,18 @@ const getOrderList = async() => {
 }
 getOrderList()
 
+// tab切换
+const tabChange = (type) => {
+    console.log(type)
+    params.value.orderState = type
+    getOrderList()
+}
+
 </script>
 
 <template>
   <div class="order-container">
-    <el-tabs>
+    <el-tabs @tab-change="tabChange">
       <!-- tab切换 -->
       <el-tab-pane v-for="item in tabTypes" :key="item.name" :label="item.label" />
 
