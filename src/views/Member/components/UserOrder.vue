72b728bc8c73a4bb6095bf21ade9a6fab8607ac5
not gloomy <2218009172.qq.com>
2023-12-18 20:50:24 +0800

    会员中心-细节优化

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -41,6 +41,18 @@ const pageChange = (page) => {
     getOrderList()
 }
 
+const fomartPayState = (payState) => {
+    const stateMap = {
+      1: '待付款',
+      2: '待发货',
+      3: '待收货',
+      4: '待评价',
+      5: '已完成',
+      6: '已取消'
+    }
+    return stateMap[payState]
+  }
+
 </script>
 
 <template>
@@ -86,7 +98,7 @@ const pageChange = (page) => {
                 </ul>
               </div>
               <div class="column state">
-                <p>{{ order.orderState }}</p>
+                <p>{{ fomartPayState(order.orderState) }}</p>
                 <p v-if="order.orderState === 3">
                   <a href="javascript:;" class="green">查看物流</a>
                 </p>
