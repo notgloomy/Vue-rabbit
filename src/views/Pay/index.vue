9ae3ccd32c0b8884b6873b68287aa64b4b725cb3
not gloomy <2218009172.qq.com>
2023-12-18 15:23:47 +0800

    支付功能实现

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -10,6 +10,14 @@ const getPayInfo = async () => {
 }
 getPayInfo()
 
+// 跳转支付
+// 携带订单id以及回调地址跳转到支付地址
+// 支付地址
+const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/'
+const backURL = 'http://127.0.0.1:5173/paycallback'
+const redirectUrl = encodeURIComponent(backURL)
+const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redirectUrl}`
+
 </script>
 
 
